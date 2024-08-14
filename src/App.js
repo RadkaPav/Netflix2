5067bd6ba6c64491630ba03e986efc007b0bcc59
Radka Pavlišová <radojka@seznam.cz>
2024-08-14 19:42:40 +0200

    Refactoring App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import React, { useState } from 'react'
 import allMovies from './data'
 import categories from './categories'
-import age from './age'
+import ageCategories from './age'
 import Movie from './components/Movie'
 
-
 const App = () => {
-  const [state, setState] = useState(true)  /*určuje, zda se vypíší filmy filtrované podle kategorie nebo podle věku */
+  const [state, setState] = useState(true)
   const [all, setAll] = useState(true)
-  //filtrování podle kategorie
   const [typeOfMovie, setTypeOfMovie] = useState("")
-  const filteredMovies = allMovies.filter((oneMovie) => {
-    return oneMovie["category"] === typeOfMovie
-  })
+  const [age, setAge] = useState("")
 
-  //filtrování podle věku
-  const [child, setChild] = useState("")
-  const ageMovies = allMovies.filter((oneMovie) => {
-    return oneMovie["age"] === child
-  })
+  const filteredCategories = allMovies.filter(oneMovie => oneMovie["category"] === typeOfMovie)
+  const filteredAge = allMovies.filter(oneMovie => oneMovie["age"] === age)
 
   return <div className='bg-black'>
-    {/* tlačítka na filtrování podle kategorie */}
+    {/*BUTTONS*/}
     <div className='flex flex-row justify-center'>
       {
         categories.map((oneCategory, index) => {
-          return <button key={index} className='w-[150px] text-white bg-[#e50914] p-1 cursor-pointer'
+          return <button
+            key={index}
+            className='w-[150px] text-white bg-[#e50914] p-1 cursor-pointer'
             onClick={() => {
               setTypeOfMovie(oneCategory)
               setState(true)
@@ -34,14 +29,14 @@ const App = () => {
         })
       }
     </div>
-
-    {/* tlačítka na filtrování podle věku */}
     <div className='flex flex-row justify-center'>
       {
-        age.map((oneAge, index) => {
-          return <button key={index} className='w-[150px] text-white bg-[#e50914] p-1 cursor-pointer'
+        ageCategories.map((oneAge, index) => {
+          return <button
+            key={index}
+            className='w-[150px] text-white bg-[#e50914] p-1 cursor-pointer'
             onClick={() => {
-              setChild(oneAge)
+              setAge(oneAge)
               setState(false)
               setAll(false)
             }}>{oneAge}</button>
@@ -49,43 +44,35 @@ const App = () => {
       }
     </div>
     <div className='flex flex-row justify-center'>
-      <button className='w-[150px] text-white bg-[#e50914] p-1 cursor-pointer'
-       onClick={() => {
-        setAll(true)
-        setChild("")
-        setTypeOfMovie("")
+      <button
+        className='w-[150px] text-white bg-[#e50914] p-1 cursor-pointer'
+        onClick={() => {
+          setAll(true)
+          setAge("")
+          setTypeOfMovie("")
         }}>Vypsat všechny filmy</button>
     </div>
 
-
-    { /* vypsání všech filmů*/
-      all ? <div className='w-[90%] flex flex-row justify-center flex-wrap my-10 mx-auto'>
+    {/* MOVIES */}
+    { 
+      all ? 
+      <div className='w-[90%] flex flex-row justify-center flex-wrap my-10 mx-auto'>
         {
-          allMovies.map((oneMovie) => {
-            return <Movie key={oneMovie.id} {...oneMovie} />
-          })
+          allMovies.map(oneMovie => <Movie key={oneMovie.id} {...oneMovie} />)
         }
       </div> : <div></div>
     }
     {
-      /* vypsání vyfiltrovaných filmů podle kategorie*/
       state ? <div className='w-[90%] flex flex-row justify-center flex-wrap my-10 mx-auto'>
         {
-          filteredMovies.map((oneMovie) => {
-            return <Movie key={oneMovie.id} {...oneMovie} />
-          })
+          filteredCategories.map(oneMovie => <Movie key={oneMovie.id} {...oneMovie} />)
         }
       </div> : <div className='w-[90%] flex flex-row justify-center flex-wrap my-10 mx-auto'>
-        { /* vypsání vyfiltrovaných filmů podle věku*/
-          ageMovies.map((oneMovie) => {
-            return <Movie key={oneMovie.id} {...oneMovie} />
-          })
+        { 
+          filteredAge.map(oneMovie => <Movie key={oneMovie.id} {...oneMovie} />)
         }
       </div>
     }
-
-
-
   </div>
 }
 
