a4111f8e3aa4b540313516ffec42550c5e89243b
Radka Pavlišová <radojka@seznam.cz>
2024-08-14 16:47:55 +0200

    Nastylováno pomocí tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import "./App.css"
 import allMovies from './data';
 import categories from './categories';
 import age from './age';
@@ -21,16 +20,16 @@ const App = () => {
     return oneMovie["age"] === child
   })
 
-  return <div>
+  return <div className='bg-black'>
     {/* tlačítka na filtrování podle kategorie */}
-    <div className='all-buttons'>
+    <div className='flex flex-row justify-center'>
       {
         categories.map( (oneCategory, index) => {
           return  <button key={index} onClick={() => {
             setTypeOfMovie(oneCategory)
             setState(true)
             setAll(false)
-            }} className='one-button'>
+            }} className='w-[150px] text-white bg-[#e50914] p-1 cursor-pointer'>
                       {oneCategory}
                   </button>
         })
@@ -38,24 +37,24 @@ const App = () => {
     </div>
     
     {/* tlačítka na filtrování podle věku */}
-    <div className='all-buttons'>
+    <div className='flex flex-row justify-center'>
       {
         age.map((oneAge, index) => {
           return <button key={index} onClick={() => {
             setChild(oneAge)
             setState(false)
             setAll(false)
-          }} className='one-button'>{oneAge}</button>
+          }} className='w-[150px] text-white bg-[#e50914] p-1 cursor-pointer'>{oneAge}</button>
         })
       }
     </div>
-    <div className='all-buttons'>
-      <button onClick={() => setAll(true)} className='one-button'>Vypsat všechny filmy</button>
+    <div className='flex flex-row justify-center'>
+      <button onClick={() => setAll(true)} className='w-[150px] text-white bg-[#e50914] p-1 cursor-pointer'>Vypsat všechny filmy</button>
     </div>
 
     
        { /* vypsání všech filmů*/
-      all? <div className='all-movies'>
+      all? <div className='w-[90%] flex flex-row justify-center flex-wrap my-10 mx-auto'>
       {
       allMovies.map((oneMovie) => {
         return <Movie key={oneMovie.id} {...oneMovie} />
@@ -63,16 +62,15 @@ const App = () => {
       }
     </div> : <div></div>
     }
-   
        {
       /* vypsání vyfiltrovaných filmů podle kategorie*/
-      state?  <div className='all-movies'>
+      state?  <div className='w-[90%] flex flex-row justify-center flex-wrap my-10 mx-auto'>
        {
       filteredMovies.map((oneMovie) => {
         return <Movie key={oneMovie.id} {...oneMovie}/>
       })
       }
-    </div> : <div className='all-movies'>
+    </div> : <div className='w-[90%] flex flex-row justify-center flex-wrap my-10 mx-auto'>
       { /* vypsání vyfiltrovaných filmů podle věku*/
        ageMovies.map((oneMovie) => {
         return <Movie key={oneMovie.id} {...oneMovie}/>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,11 @@
-import "./Movie.css"
-
 const Movie = ({image, title, age, tags, description}) => {
   return (
-    <div className="one-movie">
-        <img src={image} alt="" />
-        <h2>{title}</h2>
-        <p>{age}</p>
-        <p>{tags}</p>
-        <p>{description}</p>
+    <div className="w-[300px] text-white my-2 mx-1 transition-all duration-500 hover:scale-105" >
+        <img className="h-[170px] w-[300px]" src={image} alt="" />
+        <h2 className="pl-2 my-2">{title}</h2>
+        <p className="pl-2 my-2">{age}</p>
+        <p className="pl-2 my-2">{tags}</p>
+        <p className="pl-2 my-2">{description}</p>
     </div>
   )
 }
